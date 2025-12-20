8483ccea2c33ed845ca37dbb4eda0e0d8c94f90b
devshah95 <devshah95@gmail.com>
2025-12-20 12:25:44 -0600

    Adding practice

---
+++ demo-ec2.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "myimage" {
+    most_recent = true 
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["*"]
+    }
+}
+
+
+resource "aws_instance" "web" {
+    ami = data.aws_ami.myimage.image_id
+    instance_type = "t2.micro"
+}
